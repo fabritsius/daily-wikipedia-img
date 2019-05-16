71b2b115c9a1e1d0ae2e670abb58f5e217e9ced0
fabritsius <antonfabritsius@gmail.com>
2019-05-16 20:06:34 +0300

    feat: Add offline experience

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ func main() {
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/styles.css", stylesHandler)
 	http.HandleFunc("/favicon.ico", favIconHandler)
+	http.HandleFunc("/sw.js", serviceWorkerHandler)
 	appengine.Main()
 }
 
@@ -45,6 +46,12 @@ func stylesHandler(w http.ResponseWriter, r *http.Request) {
 // favIconHandler function handles path "/favicon.ico"
 func favIconHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "icons/wikipedia.ico")
+	fmt.Println(r.Method, r.URL)
+}
+
+// serviceWorkerHandler function handles path "/sw.js"
+func serviceWorkerHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "js/sw.js")
 	fmt.Println(r.Method, r.URL)
 }
 

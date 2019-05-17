1232ba6f9a7acc1a46af0e337b36c84b8e3daa27
Anton <antonfabritsius@gmail.com>
2019-05-17 18:10:50 +0300

    fix: Remove unnecessary complexity
    - add favicon path into list for caching
    - remove update fetching from service worker

--- sw.js
+++ sw.js
@@ -2,7 +2,8 @@ const APP_NAME = 'daily-wikipedia-img';
 const CACHE_NAME = `${APP_NAME}-v1`;
 const urlsToCache = [
   `/${APP_NAME}/`,
-  `/${APP_NAME}/index.html`
+  `/${APP_NAME}/index.html`,
+  `/${APP_NAME}/favicon.ico`	
 ];
 
 // Perform install steps
@@ -17,33 +18,17 @@ self.addEventListener('install', (event) => {
 
 // Respond with cached resources
 self.addEventListener('fetch', (event) => {
-	event.respondWith(
-	  fromCache(event.request)
-  );
-  event.waitUntil(
-	  update(event.request)
-		  .then(location.reload)
-  );
-});
-
-// Get data from the cache or fetch otherwise
-const fromCache = (request) => {
-  return caches.open(CACHE_NAME).then((cache) => {
-	  return cache.match(request).then((response) => {
-		  return response || fetch(request);
-	  });
-  })
-}
-
-// Fetch data and update the cache
-const update = (request) => {
-  return fetch(request).then((response) => {
-	  return caches.open(CACHE_NAME).then((cache) => {
-		  cache.put(request, response.clone());
-		  return response;
-	  });
-  });
-}
+  event.respondWith(
+    caches.match(event.request).then((request) => {
+      return request || fetch(event.request).then((response) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  )
+})
 
 // Delete outdated caches
 self.addEventListener('activate', (event) => {

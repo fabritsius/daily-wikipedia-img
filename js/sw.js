dd276fbb862fff0f082c025f1b356c1db843b872
fabritsius <antonfabritsius@gmail.com>
2019-05-19 16:28:50 +0300

    fix: Add auto page reload if content can be updated
    - old code was supposed to have this functionality but it did not
    - new code checks if updated index is different from cached version and asks the page to reload
    - this prevents infinite page reloads

--- js/sw.js
+++ js/sw.js
@@ -19,13 +19,14 @@ self.addEventListener('install', (event) => {
 
 // Respond with cached resources
 self.addEventListener('fetch', (event) => {
-  	event.respondWith(
+	event.respondWith(
 		fromCache(event.request)
 	);
 	event.waitUntil(
-		update(event.request)
-			.then(location.reload)
-	);
+		update(event.request).then((isUpdated) => {
+			if (isUpdated) reload();
+		})
+	)
 });
 
 // Get data from the cache or fetch otherwise
@@ -41,10 +42,37 @@ const fromCache = (request) => {
 const update = (request) => {
 	return fetch(request).then((response) => {
 		return caches.open(CACHE_NAME).then((cache) => {
-			cache.put(request, response.clone());
-			return response;
+			let isUpdated = false;
+			// check if new index page is different from what is in the cache
+			return cache.match('/').then((indexCache) => {
+				if (response.url === indexCache.url) {
+					return Promise.all([
+						response.clone().text(),
+						indexCache.text()
+					]);
+				}
+				return Promise.resolve(false);
+			}).then((indexVersions) => {
+				if (indexVersions) {
+					const [newIndex, oldIndex] = indexVersions;
+					if (newIndex !== oldIndex) {
+						isUpdated = true;
+					}
+				}
+				return cache.put(request, response);				
+			}).then(() => {
+				return Promise.resolve(isUpdated);
+			});
 		});
 	});
+}
+
+
+// Sent a message to reload the window
+const reload = () => {
+	self.clients.matchAll().then((clients) => {
+		clients.forEach((client) => client.postMessage('reload-window'));
+    });
 }
 
 // Delete outdated caches

4081ec24ad192600aba1a5c50a00a2d2def07146
fabritsius <antonfabritsius@gmail.com>
2019-07-27 18:37:43 +0300

    fix: Add scroll top for mobile
    scrolling isn't smooth on mobile

--- js/scroll-top.js
+++ js/scroll-top.js
@@ -1,11 +1,14 @@
 const navText = document.querySelector('nav h1');
 const content = document.querySelector('div.body-filler');
 
-document.addEventListener('click', (event) => {
+document.addEventListener('click', (event) => scrollTop(event));
+document.addEventListener('touchstart', (event) => scrollTop(event));
+
+const scrollTop = (event) => {
     if (event.target === navText) {
         content.scrollTo({
             top: 0,
             behavior: 'smooth'
         });
     }
-});
+}
